977ac6eae615e8bc5e7331cecdf9152360eca1b2
andre <andre.aldeia@yahoo.com.br>
2017-08-08 17:07:35 -0300

    falta testar o campo vazio

--- pn.sh
+++ pn.sh
@@ -1,12 +1,16 @@
 #!/bin/bash 
 ###################### script testa se o número é  positivo ou negativo
 numero="numero"
-  echo "Entre com o número"
+  echo "Entre com o número:"
    read $numero
 
-      if [ $numero -ge 0 ];then
-  echo Número Positivo $numero
+# if [-z $numero ]; then  #testa se o campo esta vazio
+# echo Não pode ser vázio
+
+
+      if [ $numero -ge 0 ];then  #testa se e igual ou maior que 0
+  echo Número Positivo $numero !
 else
-  echo Número Negativo $numero
+  echo Número Negativo $numero ! #mostra se for abaixo de 0
 fi
 
