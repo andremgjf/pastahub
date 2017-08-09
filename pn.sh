6dc7ce1ed056bfbf58acbbc87021545d2724e57f
andre <andre.aldeia@yahoo.com.br>
2017-08-09 10:49:27 -0300

    concluído: testa se determinado número e positivo ou negativo

--- pn.sh
+++ pn.sh
@@ -1,16 +1,19 @@
+
 #!/bin/bash 
 ###################### script testa se o número é  positivo ou negativo
 numero="numero"
   echo "Entre com o número:"
    read $numero
 
-# if [-z $numero ]; then  #testa se o campo esta vazio
-# echo Não pode ser vázio
+#testar se o campo está vázio
+ if [ -z $numero ]; then
+ echo Não pode ser vázio
 
 
-      if [ $numero -ge 0 ];then  #testa se e igual ou maior que 0
+ elif [ $numero -ge 0 ];then  #testa se e igual ou maior que 0
   echo Número Positivo $numero !
-else                            #testa se e negativo
+
+else                           #testa se e negativo
   echo Número Negativo $numero ! 
 fi
 
