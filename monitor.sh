6301d604780f1c2108d573782dad9f667b7bfe4d
andre <andre.aldeia@yahoo.com.br>
2017-09-11 14:19:25 -0300

    monitor

--- monitor.sh
+++ monitor.sh
@@ -5,6 +5,7 @@ cm="cm"
 echo "Entre com os cm da tela"
  read $cm
 
-total=$[ cm/2.54 ]
-
-echo "Tela de $total Polegadas"
+#total=$[ cm/2.54 ]
+total=$[ cm/2 |bc ] 
+echo $total
+#echo "Tela de $total Polegadas"
