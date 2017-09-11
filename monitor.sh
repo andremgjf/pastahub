88491b6c609e083f8325a1fe263e65093a0744e4
andre <andre.aldeia@yahoo.com.br>
2017-09-11 15:21:31 -0300

    monitor

--- monitor.sh
+++ monitor.sh
@@ -1,11 +1,19 @@
+#Converterá os centímetros digitado em polegadas
+#Cada polegada equivale a 2.54
+###.............................................###
+
 
 #!/bin/bash
 cm="cm"
 
-echo "Entre com os cm da tela"
+echo "Entre com os centímetros da tela"
  read $cm
+clear
 
 #total=$[ cm/2.54 ]
-total=$[ cm/2 |bc ] 
+#total=$[ cm+2,1 | bc]  # calcular número com ponto flutuante
+echo $total
+#"scale=2; cm*2" | bc
 
-echo "Tela de $total Polegadas"
+
+#echo "Tela de $total Polegadas"
