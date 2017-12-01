5ef00b49308a17569a140edd111e32a5d68ea056
andre <andre.aldeia@yahoo.com.br>
2017-12-01 10:42:41 -0200

    lembrete.sh

--- lembrete.sh
+++ lembrete.sh
@@ -1,12 +1,13 @@
-#calendário
+#Lembrete semanal usando o calendário do shell
 
 #!/bin/bash
 
 data="date +%a:%B"
+dia="date +%d"
 $data
+$dia
 
+#if [ "$data"="Sex"and $dia +7=mesatual ]; then
+# echo "Amanhã tem compromisso!!!!"
 
-if [ "$data"="Sex" ]; then
- echo "hoje e sexta-feira"
-
-fi
+#fi
